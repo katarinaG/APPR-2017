4ea39b18142645a0274a666365c91af532fe4b00
Katarina Gacnik <katarina.gacnik@student.fmf.uni-lj.si>
2017-08-28 02:17:07 +0200

    Graf #2 - nedokoncan

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -3,6 +3,7 @@
 library(ggplot2)
 library(dplyr)
 library(reshape2)
+library(tidyr)
 
 #Naredim tabelo, v kateri glede na spol in leto prikazem stevilo prijavljenih in povprecen cas
 
@@ -28,15 +29,20 @@ ggplot(stevilo_sodelujocih, aes(x = Year, y = Stevilo_prijav, fill = Gender)) + 
   scale_fill_discrete(name="Spol", labels = c("Ženske", "Moški", "Ni podatka")) + theme_minimal() +
   geom_text(aes(y= Stevilo_prijav, label= Stevilo_prijav), vjust=0.1, color="black", size=3.5) +
   theme(plot.title = element_text(hjust = 0.5)) +
-  scale_x_continuous(breaks = seq(2003, 2017, by =1))
+  scale_x_continuous(breaks = seq(2003, 2017, by =1)) 
+  
+
+#Graf, ki prikaze stevilo odstopov
 
 
 
-
-
-
-
-
-
+razmerje_uspeh <- data.frame(stevilo_sodelujocih_vsi$Stevilo_prijav_vsi, stevilo_sodelujocih_vsi$Koncali, stevilo_sodelujocih_vsi$Year)
+names(razmerje_uspeh) <- c( "Vsi", "Koncali", "Leto")
+gather(razmerje_uspeh, key, value, Vsi, Koncali) %>%
+  ggplot(aes(x=Leto, y=value, colour=key)) +
+  geom_line(size =1) + ggtitle("Število odstopov skozi čas") + theme(legend.position = "right") +
+  theme_minimal() + theme(plot.title = element_text(hjust = 0.5)) +
+  scale_x_continuous(breaks = seq(2003, 2017, by =1)) + ylab("Število tekmovalcev") +
+  scale_y_continuous(breaks = seq(0, 200, by =25)) 
 
 
